86174575033c406b39ac924fcfa017805a8dd040
marksampson <mark.sampson@nimbleapproach.com>
2024-11-19 18:47:42 +0000

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# usecure Tech Test
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="text-center text-2xl font-bold text-blue-500">
+      Welcome to React + Vite + Tailwind + TypeScript!
+    </div>
+  );
+};
+
+export default App;
